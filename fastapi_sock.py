8be9be4bef1eaa0a13034089c2c3609d028ca479
m-m-kh-py <khademim217@gmail.com>
2024-04-21 14:43:50 +0330

    U2

--- fastapi_sock.py
+++ fastapi_sock.py
@@ -1,11 +1,72 @@
 from fastapi import FastAPI, WebSocket
+from fastapi.responses import Response, HTMLResponse
+from fastapi.requests import Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import asyncio
+import pathlib
 
+template = Jinja2Templates(pathlib.Path(__file__).parent/'templates')
 
 app = FastAPI()
 
+app.mount('/static',StaticFiles(directory='static'),name='static')
+
+
+app.sockets = set() 
+
+
+async def check_socket(tasks, websocket: WebSocket):
+    while websocket.client_state.name == "CONNECTED":
+        await asyncio.sleep(5)
+    
+    for task in tasks:
+        task:asyncio.Task
+        task.cancel()
+    
+        
+        
+
+async def reciver(websocket: WebSocket):
+    while True:
+        print(await websocket.receive_text())
+    
+        
+async def sender(websocket: WebSocket):
+    while True:
+        txt = await asyncio.to_thread(input,':>>>>>>> ')
+        print(await websocket.send_text(txt))
+
+
+async def worker(websocket: WebSocket):
+    t1 = asyncio.create_task(reciver(websocket))
+    t2 = asyncio.create_task(sender(websocket))
+    t3 = asyncio.create_task(check_socket([t1,t2],websocket))
+
+    await asyncio.gather(t1,t2,t3)
+
+
+    
+    
 @app.websocket('/ws')
 async def websocket_entry(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        print(await websocket.receive())
-        await websocket.send_text('w')
+    await websocket.accept()  
+    websocket.app.sockets.add(websocket)  
+    await worker(websocket)
+    
+    
+    
+@app.route('/')
+async def view_sockets(request:Request):
+    
+    discoonect = set()
+    for sock in request.app.sockets:
+        if sock.client_state.name != "CONNECTED":
+            discoonect.add(sock)
+    sockets = request.app.sockets.difference(discoonect)
+            
+    
+    return template.TemplateResponse('index.html',
+                                     context={
+                                         'request':request,
+                                         'sockets':sockets})
--- js_reciver.js
+++ js_reciver.js
@@ -1,19 +1,37 @@
-const socket = new WebSocket('ws://localhost:8765');
+const socket = new WebSocket('ws://localhost:8000/ws');
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+const h1 = document.querySelector('h1')
+
+
+
+button.addEventListener('click',e =>{
+    socket.send(input.value)
+})
+
+
+
+
 
 socket.addEventListener('open', function (event) {
     console.log('Connected to WebSocket server');
 });
 
 socket.addEventListener('message', function (event) {
-    console.log('Message from server:', event.data);
+    console.log(event.data);
+    h1.innerHTML = event.data
     // socket.send('Hello, WebSocket Server!');
 });
 
-socket.addEventListener('close', function (event) {
-    console.log('Disconnected from WebSocket server');
-});
+// socket.addEventListener('close', function (event) {
+//     console.log('Disconnected from WebSocket server');
+// });
 
 
-socket.addEventListener('error', function (event) {
-    console.error('WebSocket error:', event);
-});
+// socket.addEventListener('error', function (event) {
+//     console.error('WebSocket error:', event);
+// });
+
+// addEventListener("beforeunload", e => {
+//     socket.close()
+// })
